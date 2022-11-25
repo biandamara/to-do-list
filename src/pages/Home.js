1e427353325535953722bd130636dca13566ed58
Bian Damara <bian.damara@gmail.com>
2022-11-25 13:58:21 +0700

    edit home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,9 @@
 import React from "react";
 
-// impor file css
+// import components
+import List from "../component/List";
+
+// import file css
 import style from "../assets/styles/Home.module.css";
 
 function Home() {
@@ -8,7 +11,20 @@ function Home() {
     <body className={`${style.body}`}>
       {/* card - start */}
       <div className={`container text-center ${style.card}`}>
+        {/* title - start */}
         <h1>To-do List</h1>
+        {/* title - end */}
+
+        {/* form - start */}
+        <form>
+          <input type={"text"} placeholder={"Enter task"}></input>
+          <button type={"submit"}>Add</button>
+        </form>
+        {/* form - end */}
+
+        {/* list - start */}
+        <List></List>
+        {/* list - end */}
       </div>
       {/* card - end */}
     </body>
