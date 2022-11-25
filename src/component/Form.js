f54066790517516e9785bb4dbc9529c32e8fc4a8
Bian Damara <bian.damara@gmail.com>
2022-11-25 18:31:10 +0700

    edit component form and task

--- src/component/Form.js
+++ src/component/Form.js
@@ -1,7 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Form() {
-  return <body></body>;
+// import package
+import { v4 as uuidv4 } from "uuid";
+
+function Form(addTask) {
+  const [task, setTask] = useState({
+    id: "",
+    task: "",
+    completed: false,
+  });
+
+  // function handle input task
+  function handleTaskInput(e) {
+    // e.target.value contains new input from onChange
+    // event for input elements
+    setTask({
+      ...task,
+      task: e.target.value,
+    });
+  }
+
+  // function handle submit task
+  function handleSubmitTask(e) {
+    // prevents browser refresh
+    // trim() gets rid of string whitespace
+    e.preventDefault();
+    if (task.task.trim()) {
+      addTask({ ...task, id: uuidv4() });
+      setTask({ ...task, task: "" });
+    }
+  }
+
+  return (
+    <body>
+      {/* form - start */}
+      <form onSubmit={handleSubmitTask}>
+        {/* input text - start */}
+        <input
+          name={`task`}
+          type={`text`}
+          value={task.task}
+          onChange={handleTaskInput}
+        ></input>
+        {/* input text - end */}
+
+        {/* submit button - start */}
+        <button type="submit">Submit</button>
+        {/* submit button - end */}
+      </form>
+      {/* form - end */}
+    </body>
+  );
 }
 
 export default Form;
--- src/component/Task.js
+++ src/component/Task.js
@@ -1,7 +1,49 @@
 import React from "react";
 
-function Task() {
-  return <body></body>;
+function Task(task, handleCompleted, handleDelete) {
+  // function handle checkbox if the task is complete
+  function checkboxButton() {
+    handleCompleted(task.id);
+  }
+
+  // function handle delete if the task doesn't complete
+  function deleteButton() {
+    handleDelete(task.id);
+  }
+
+  return (
+    <body>
+      <ul>
+        {/* checkbox button - start */}
+        <li>
+          <input
+            type={`checkbox`}
+            checked={task.completed}
+            onClick={checkboxButton}
+          ></input>
+        </li>
+        {/* checkbox button - end */}
+
+        {/* tasks - start */}
+        <li>
+          <p
+            style={{
+              textDecoration: task.completed ? "line-through" : null,
+            }}
+          >
+            {task.task}
+          </p>
+          {/* tasks - end */}
+        </li>
+
+        {/* delete button - start */}
+        <li>
+          <button onClick={deleteButton}>X</button>
+        </li>
+        {/* delete button - end */}
+      </ul>
+    </body>
+  );
 }
 
 export default Task;
