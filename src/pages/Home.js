7009976f60636a822e8ad590ccb8a1f655f23c2c
Bian Damara <bian.damara@gmail.com>
2022-11-28 18:38:46 +0700

    edit home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,48 @@
-import React from "react";
+import React, { Component } from "react";
 
 // import styles
 import style from "../assets/styles/Home.module.css";
 
-function Home() {
-  return (
-    <body>
-      {/* input task form - start */}
-      <div className={`${style.form}`}>
-        <form>
-          <input className={`${style.input}`} placeholder={`Add Task`} />
-          <button className={`${style.submit}`} type={`submit`}>
-            Add
-          </button>
-        </form>
-        {/* input task form - end */}
-      </div>
-    </body>
-  );
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { tasks: [] };
+    this.addTask = this.addTask.bind(this);
+    this.removeTask = this.removeTask.bind(this);
+  }
+
+  addTask(task) {
+    this.setState((prevState) => {
+      return {
+        tasks: prevState.tasks.concat(task),
+      };
+    });
+  }
+
+  removeTask(task) {
+    this.setState((prevState) => {
+      return {
+        tasks: prevState.tasks.filter((t) => t !== task),
+      };
+    });
+  }
+
+  render() {
+    return (
+      <body>
+        {/* input task form - start */}
+        <div className={`${style.form}`}>
+          <form>
+            <input className={`${style.input}`} placeholder={`Add Task`} />
+            <button className={`${style.submit}`} type={`submit`}>
+              Add
+            </button>
+          </form>
+          {/* input task form - end */}
+        </div>
+      </body>
+    );
+  }
 }
 
 export default Home;
