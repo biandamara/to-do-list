4f283c8ef3fe776a19c1e45e246549ef628fe313
Bian Damara <bian.damara@gmail.com>
2022-11-28 09:53:25 +0700

    edit home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,20 @@
 import React from "react";
 
 function Home() {
-  return <body></body>;
+  return (
+    <body>
+      {/* input task form - start */}
+      <div className="form">
+        <form>
+          <input className="input" placeholder="Add Task" />
+          <button className="add-button" type="submit">
+            Add
+          </button>
+        </form>
+        {/* input task form - end */}
+      </div>
+    </body>
+  );
 }
 
 export default Home;
