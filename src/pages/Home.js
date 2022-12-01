bf0e346ade9debc216694222e485d292c54ed98d
Bian Damara <bian.damara@gmail.com>
2022-12-01 20:47:48 +0700

    edit home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -46,41 +46,43 @@ function Home() {
   }
 
   return (
-    <body className={`container ${style.body}`}>
+    <body className={`${style.body}`}>
       <div className={`container`}>
-        {/* card - start */}
-        <div className={`card ${style.card}`}>
-          <div className="card-body mx-3">
-            {/* header - start */}
-            <div className={`text-center ${style.header}`}>
-              <img src="../../public/icon.png" alt=""></img>
-              <h1>To-do List</h1>
-            </div>
-            {/* header - end */}
+        <div className={`container`}>
+          {/* card - start */}
+          <div className={`card ${style.card}`}>
+            <div className="card-body mx-3">
+              {/* header - start */}
+              <div className={`text-center ${style.header}`}>
+                <img src="../../public/icon.png" alt=""></img>
+                <h1>To-do List</h1>
+              </div>
+              {/* header - end */}
 
-            {/* form - start */}
-            <div className={`my-4`}>
-              <Form addTodo={addTodo} />
-            </div>
-            {/* form - end */}
+              {/* form - start */}
+              <div className={`my-4`}>
+                <Form addTodo={addTodo} />
+              </div>
+              {/* form - end */}
 
-            {/* task remaining - start */}
-            <div className={`text-center`}>
-              {/* <p>Task remaining ()</p> */}
-            </div>
-            {/* task remaining - end */}
+              {/* task remaining - start */}
+              <div className={`text-center`}>
+                {/* <p>Task remaining ()</p> */}
+              </div>
+              {/* task remaining - end */}
 
-            {/* to-do list - start */}
-            <div className={`my-4`}>
-              <List
-                todos={todos}
-                removeTodo={removeTodo}
-                toggleComplete={toggleComplete}
-              />
+              {/* to-do list - start */}
+              <div className={`my-4`}>
+                <List
+                  todos={todos}
+                  removeTodo={removeTodo}
+                  toggleComplete={toggleComplete}
+                />
+              </div>
+              {/* to-do list - end */}
             </div>
-            {/* to-do list - end */}
+            {/* card - end */}
           </div>
-          {/* card - end */}
         </div>
       </div>
     </body>
